efcc3507d50a03b26641ff40cbbbef2d259e157f
stijnvanhoey <stijnvanhoey@gmail.com>
2017-12-22 15:45:09 +0100

    Improve documentation

--- gdrive_plugin/gdrive_download_cli.py
+++ gdrive_plugin/gdrive_download_cli.py
@@ -18,7 +18,10 @@ from gdrive_interaction import GDriveConnect
 @click.option('--nlim', default=None, 
               help='Limit the number of files to check for each deployment')
 def download_gdrive(credentials, gdrive_hash, local_dir, nlim):
-    """Download all deployments from a googl drive folder
+    """
+    Download all deployments from a google drive folder, using the 
+    google API CREDENTIALS file for the connection and a LOCAL_DIR
+    to copy data to
     """
     gconnect = GDriveConnect(cred_file=credentials,
                              folder_hash=gdrive_hash)
